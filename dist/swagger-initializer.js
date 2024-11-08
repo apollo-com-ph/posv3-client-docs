e03dca5662502273c824dd32417fb8b8c7e64a95
ian <ian@apollo.com.ph>
2024-11-08 16:18:40 +0800

    refactor

--- dist/swagger-initializer.js
+++ dist/swagger-initializer.js
@@ -3,7 +3,7 @@ window.onload = function() {
 
   // the following lines will be replaced by docker/configurator, when it runs in a docker-container
   window.ui = SwaggerUIBundle({
-    url: "https://raw.githubusercontent.com/apollo-com-ph/posv3-client-docs/refs/heads/main/posv3-client-openapi.yml",
+    url: "posv3-client-openapi.yaml",
     dom_id: '#posv3-client-api',
     deepLinking: true,
     presets: [
